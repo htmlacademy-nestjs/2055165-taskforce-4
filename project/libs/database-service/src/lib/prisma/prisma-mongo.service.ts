c281f4d52d874369f6c191a5095257be87e6c56b
Godskitchen <diemygod@gmail.com>
2023-08-16 23:18:27 +0300

    fix: prisma connectors

--- project/libs/database-service/src/lib/prisma/prisma-mongo.service.ts
+++ project/libs/database-service/src/lib/prisma/prisma-mongo.service.ts
@@ -3,6 +3,17 @@ import { PrismaClient } from '../../../../../node_modules/@internal/prisma/mongo
 
 @Injectable()
 export class PrismaMongoService extends PrismaClient implements OnModuleInit {
+
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: "mongodb://admin:pass@localhost:27017/taskforce?authSource=admin"
+        }
+      }
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
--- project/libs/database-service/src/lib/prisma/prisma-postgres.service.ts
+++ project/libs/database-service/src/lib/prisma/prisma-postgres.service.ts
@@ -3,6 +3,16 @@ import { PrismaClient } from '../../../../../node_modules/@internal/prisma/postg
 
 @Injectable()
 export class PrismaPostgresService extends PrismaClient implements OnModuleInit {
+
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: "postgresql://admin:pass@localhost:6500/taskforce?schema=public"
+        }
+      }
+    });
+  }
   async onModuleInit() {
     await this.$connect();
   }
