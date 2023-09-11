a06de53f1c16ae8affd600aa0fc8c5df593868c7
Godskitchen <diemygod@gmail.com>
2023-09-11 23:28:29 +0300

    fix

--- project/apps/api-gateway/src/app/services/authentication/dto/login-user.dto.ts
+++ project/apps/api-gateway/src/app/services/authentication/dto/login-user.dto.ts
@@ -1,10 +1,4 @@
-import { IsEmail, IsEmpty, IsString } from 'class-validator';
-
 export default class LoginUserDTO {
-  @IsEmail()
   public email!: string;
-
-  @IsString()
-  @IsEmpty()
   public password!: string;
 }
--- project/apps/api-gateway/src/app/services/tasks/dto/pin-task.dto.ts
+++ project/apps/api-gateway/src/app/services/tasks/dto/pin-task.dto.ts
@@ -1,5 +1,3 @@
-import { IsMongoId } from "class-validator";
-
 export default class PinTaskDTO {
   public executorId!: string;
 }
